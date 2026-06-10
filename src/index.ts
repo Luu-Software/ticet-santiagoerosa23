f33631633993af60db899f605c0690c6a8a0c307
santiagoerosa23 <santiago.erosa23@gmail.com>
2026-06-10 16:01:13 -0300

    Corregir y terminar para que el calculo total multiplique por la cantidad de entradas y aplicar descuento

--- src/index.ts
+++ src/index.ts
@@ -21,13 +21,36 @@ DARIO       |       50%
 */
 
 // COMPLETAR: Implementar la función calcularTotal que reciba el id del artista, la cantidad de entradas y un código de descuento (opcional) y devuelva el precio total a pagar en patacones.
-
+ 
 function calcularTotal(artista:string, cantidad:number, codigo:string):number{
   
-  
+  let preciobase:number = 0;
 
+  if (artista === 'sabrina') {
+    preciobase = 1000;
+
+else if (artista === 'kgatlw') {
+    preciobase = 700;
+    else if (artista === 'lali') {
+      preciobase = 500;
+      else if (artista === 'magdalena') {
+        preciobase = 600;
+        else if (artista === 'viagra') {
+          preciobase = 400;
+          else if (artista === 'dillom') {
+            preciobase = 350;
+            else if (artista === 'marilina') {
+              preciobase = 200;
+              else if (artista === 'mugre') {
+                preciobase = 150;
+              }
+            }
+          }
+        }
+      }
+    
+}}}
 }
-
 cuandoPasa('seleccionarArtista', ({ id, cantidad, codigoDescuento }) => {
   let cantidadNum: number = Number(cantidad);
   let precio: number = calcularTotal(id, cantidadNum, codigoDescuento);
